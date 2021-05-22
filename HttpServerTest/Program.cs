8b467156ff95c5ef8a1a78d1184732d44b68a469
Robin <robin@example.com>
2021-05-22 21:19:44 +0800

    初版server新增

--- HttpServerTest/Program.cs
+++ HttpServerTest/Program.cs
@@ -4,22 +4,32 @@ using System.Net;
 using System.Text;
 using System.Threading;
 using System.Linq;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
 
 namespace HttpServerTest {
     class Program {
+        #region 聲量控制
+        //運用WinAPI控制電腦靜音與音量
+        private const int APPCOMMAND_VOLUME_MUTE = 0x80000;
+        private const int APPCOMMAND_VOLUME_UP = 0x0a0000;
+        private const int APPCOMMAND_VOLUME_DOWN = 0x090000;
+        private const int WM_APPCOMMAND = 0x319;
+        //private static IntPtr Handle;
+        public static Process p = Process.GetCurrentProcess();
+        [DllImport("user32.dll")]
+        public static extern IntPtr SendMessageW(IntPtr hWnd, int Msg, IntPtr wParam, IntPtr lParam);
+        #endregion
+
         public static string SendResponse(HttpListenerRequest request) {
-            //return string.Format("<HTML><BODY>My web page.<br>{0}</BODY></HTML>", DateTime.Now);
             Console.WriteLine("[{0}] {1}", DateTime.Now.ToString("yyyy/dd/MM HH:mm:ss"), request.Url.OriginalString);
+            //return string.Format("<HTML><BODY>My web page.<br>{0}</BODY></HTML>", DateTime.Now.ToString("yyyy/dd/MM HH:mm:ss"));
 
             string r = "";
             foreach (string key in request.QueryString.AllKeys)
                 r += string.Format("{0}={1}{2}", key, request.QueryString[key], request.QueryString.AllKeys.ToList().IndexOf(key) != request.QueryString.AllKeys.Length - 1 ? "\n" : "");
             Console.WriteLine(r);
-
             ParseArgs(r);
-
-            //System.Diagnostics.Process.Start("http://google.com");
-
             return string.Format("Test web server: {0}", DateTime.Now.ToString("yyyy/dd/MM HH:mm:ss"));
         }
 
@@ -29,22 +39,47 @@ namespace HttpServerTest {
             string value = s.Split('=')[1];
 
             if (key == "browser") {
-                System.Diagnostics.Process.Start("http://" + value);
+                Process.Start("http://" + value);
+            } else if (key == "volumn") {
+                switch (value) {
+                    case "up":
+                        // 聲音變大 
+                        SendMessageW(p.Handle, WM_APPCOMMAND, p.Handle, (IntPtr)APPCOMMAND_VOLUME_UP);
+                        break;
+                    case "down":
+                        // 聲音變小 
+                        SendMessageW(p.Handle, WM_APPCOMMAND, p.Handle, (IntPtr)APPCOMMAND_VOLUME_DOWN);
+                        break;
+                    case "mute":
+                        // 靜音 
+                        SendMessageW(p.Handle, WM_APPCOMMAND, p.Handle, (IntPtr)APPCOMMAND_VOLUME_MUTE);
+                        break;
+                }
+            } else if (key == "close") {
+                switch (value) {
+                    case "chrome":
+                        Process[] myProcesses = Process.GetProcesses();
+                        foreach (Process myProcess in myProcesses) {
+                            if (myProcess.ProcessName.ToUpper() == "CHROME") {
+                                myProcess.Kill();
+                            }
+                        }
+                        break;
+                }
             }
         }
 
-        public static object GetComportNames() {
-            return new string[] { "COM1", "COM2", "COM3" };
-        }
-
         static void Main(string[] args) {
-            //var ws = new WebServer(SendResponse, "http://localhost:8080/test/");
-            var ws = new WebServer(SendResponse, "http://127.0.0.1:8080/");
-            //var ws = new WebServer(SendResponse, "http://192.168.1.17/");
+            var ws = new WebServer(SendResponse, "http://192.168.1.17:8080/");
+            //var ws = new WebServer(SendResponse, "http://localhost:8081/");
+            //var ws = new WebServer(SendResponse, "http://127.0.0.1:8081/");
             ws.Run();
             Console.WriteLine("Press a key to quit.");
             Console.ReadKey();
             ws.Stop();
+
+            //Console.WriteLine(1);
+            //Console.ReadLine();
         }
     }
 
@@ -57,7 +92,7 @@ namespace HttpServerTest {
                 throw new NotSupportedException("Needs Windows XP SP2, Server 2003 or later.");
             }
 
-            // URI prefixes are required eg: "http://localhost:8080/test/"
+            // URI prefixes are required eg: "http://localhost:8080/"
             if (prefixes == null || prefixes.Count == 0) {
                 throw new ArgumentException("URI prefixes are required");
             }
@@ -74,18 +109,14 @@ namespace HttpServerTest {
             _listener.Start();
         }
 
-        public WebServer(Func<HttpListenerRequest, string> method, params string[] prefixes)
-           : this(prefixes, method) {
-        }
+        public WebServer(Func<HttpListenerRequest, string> method, params string[] prefixes) : this(prefixes, method) { }
 
         public void Run() {
-            ThreadPool.QueueUserWorkItem(o =>
-            {
+            ThreadPool.QueueUserWorkItem(o => {
                 Console.WriteLine("Webserver running...");
                 try {
                     while (_listener.IsListening) {
-                        ThreadPool.QueueUserWorkItem(c =>
-                        {
+                        ThreadPool.QueueUserWorkItem(c => {
                             var ctx = c as HttpListenerContext;
                             try {
                                 if (ctx == null) {
